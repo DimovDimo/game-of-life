ab7f4922b8ab9040b8a6b7ab38af1d71f582f56a
DimovDimo <d.dimov1996@gmail.com>
2020-03-20 17:01:51 +0200

    added getCells

--- app/resources/js/game-of-life.js
+++ app/resources/js/game-of-life.js
@@ -1,6 +1,13 @@
+const cellConstant = 3;
+
 class Engine {
     constructor(width, height) {
         this.width = width;
         this.height = height;
+        this.cells = this.getCells(width, height);
+    }
+
+    getCells(width, height){
+        return new Array(width * height);
     }
 }
